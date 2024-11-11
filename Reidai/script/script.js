647bc3d00e8b49de154181e9f48c34c8168ece05
aso-riku <2401334@s.asojuku.ac.jp>
2024-11-11 12:31:57 +0900

    修正

--- Reidai/script/script.js
+++ Reidai/script/script.js
@@ -1,16 +1,22 @@
 // JavaScript用ファイル
 new Vue({
     el: '#app',
-    
     data: {
+      password1: '',
+      password2: ''
     },
     computed: {
       // 入力されている、且つパスワードが一致しているかを確認する算出プロパティ
+      passwordMismatch() {
+        return this.password1 && this.password2 && this.password1 !== this.password2; 
+      },
       // 入力されているかは、this.･･･ で判定可能
       // 例）一致TRUE（パスワード：aaaa、パスワード確認：aaaa）
       // 例）不一致FALSE（パスワード：aaaa、パスワード確認：aaab）
 
       // ボタンをTRUEなら登録を表示orFALSEなら使用不可を表示にする算出プロパティ
-
+      canRegister() {
+        return this.password1 && this.password2 && this.password1 === this.password2;
+      }
     }
   });
